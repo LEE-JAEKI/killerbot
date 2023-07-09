198054fd123f0b5e7e556331c621f49883f56764
LEE-JAEKI <45660776+LEE-JAEKI@users.noreply.github.com>
2023-07-09 17:26:37 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1301,43 +1301,43 @@ class mainCog(commands.Cog):
 			await ctx.send(f"< 텍스트채널 [{ctx.message.channel.name}] 접속완료 >\n< 음성채널 접속 후 [{command[5][0]}] 명령을 사용 하세요 >", tts=False)
 			
 			print('< 텍스트채널 [' + ctx.guild.get_channel(basicSetting[7]).name + '] 접속완료>')
-			if basicSetting[6] != "" and basicSetting[21] == "1":
-				try:
-					await ctx.guild.get_channel(basicSetting[6]).connect(reconnect=True, timeout=5)
-					print('< 음성채널 [' + ctx.guild.get_channel(basicSetting[6]).name + '] 접속완료>')
-				except:
-					print('< 음성채널 [' + ctx.guild.get_channel(basicSetting[6]).name + '] 접속에러! >')
-					pass
-			if basicSetting[8] != "":
-				if str(basicSetting[8]) in channel_id:
-					print('< 사다리채널 [' + ctx.guild.get_channel(int(basicSetting[8])).name + '] 접속완료 >')
-				else:
-					basicSetting[8] = ""
-					print(f"사다리채널 ID 오류! [{command[28][0]} 사다리] 명령으로 재설정 바랍니다.")
-			if basicSetting[11] != "":
-				if str(basicSetting[11]) in channel_id:
-					print('< 정산채널 [' + ctx.guild.get_channel(int(basicSetting[11])).name + '] 접속완료>')
-				else:
-					basicSetting[11] = ""
-					print(f"정산채널 ID 오류! [{command[28][0]} 정산] 명령으로 재설정 바랍니다.")
-			if basicSetting[18] != "":
-				if str(basicSetting[18]) in channel_id:
-					print('< 척살채널 [' + ctx.guild.get_channel(int(basicSetting[18])).name + '] 접속완료>')
-				else:
-					basicSetting[18] = ""
-					print(f"척살채널 ID 오류! [{command[28][0]} 척살] 명령으로 재설정 바랍니다.")
+			#if basicSetting[6] != "" and basicSetting[21] == "1":
+			#	try:
+			#		await ctx.guild.get_channel(basicSetting[6]).connect(reconnect=True, timeout=5)
+			#		print('< 음성채널 [' + ctx.guild.get_channel(basicSetting[6]).name + '] 접속완료>')
+			#	except:
+			#		print('< 음성채널 [' + ctx.guild.get_channel(basicSetting[6]).name + '] 접속에러! >')
+			#		pass
+			#if basicSetting[8] != "":
+			#	if str(basicSetting[8]) in channel_id:
+			#		print('< 사다리채널 [' + ctx.guild.get_channel(int(basicSetting[8])).name + '] 접속완료 >')
+			#	else:
+			#		basicSetting[8] = ""
+			#		print(f"사다리채널 ID 오류! [{command[28][0]} 사다리] 명령으로 재설정 바랍니다.")
+			#if basicSetting[11] != "":
+			#	if str(basicSetting[11]) in channel_id:
+			#		print('< 정산채널 [' + ctx.guild.get_channel(int(basicSetting[11])).name + '] 접속완료>')
+			#	else:
+			#		basicSetting[11] = ""
+			#		print(f"정산채널 ID 오류! [{command[28][0]} 정산] 명령으로 재설정 바랍니다.")
+			#if basicSetting[18] != "":
+			#	if str(basicSetting[18]) in channel_id:
+			#		print('< 척살채널 [' + ctx.guild.get_channel(int(basicSetting[18])).name + '] 접속완료>')
+			#	else:
+			#		basicSetting[18] = ""
+			#		print(f"척살채널 ID 오류! [{command[28][0]} 척살] 명령으로 재설정 바랍니다.")
 			if basicSetting[19] != "":
 				if str(basicSetting[19]) in channel_id:
 					print('< 경주채널 [' + ctx.guild.get_channel(int(basicSetting[19])).name + '] 접속완료>')
 				else:
 					basicSetting[19] = ""
 					print(f"경주채널 ID 오류! [{command[28][0]} 경주] 명령으로 재설정 바랍니다.")
-			if basicSetting[20] != "":
-				if str(basicSetting[20]) in channel_id:
-					print('< 아이템채널 [' + ctx.guild.get_channel(int(basicSetting[20])).name + '] 접속완료>')
-				else:
-					basicSetting[20] = ""
-					print(f"아이템채널 ID 오류! [{command[28][0]} 아이템] 명령으로 재설정 바랍니다.")
+			#if basicSetting[20] != "":
+			#	if str(basicSetting[20]) in channel_id:
+			#		print('< 아이템채널 [' + ctx.guild.get_channel(int(basicSetting[20])).name + '] 접속완료>')
+			#	else:
+			#		basicSetting[20] = ""
+			#		print(f"아이템채널 ID 오류! [{command[28][0]} 아이템] 명령으로 재설정 바랍니다.")
 			if int(basicSetting[13]) != 0 :
 				print('< 보탐봇 재시작 시간 ' + endTime.strftime('%Y-%m-%d ') + endTime.strftime('%H:%M:%S') + ' >')
 				print('< 보탐봇 재시작 주기 ' + basicSetting[13] + '일 >')
@@ -2842,9 +2842,9 @@ class mainCog(commands.Cog):
 			random_pos = []
 			racing_result = []
 			output = ':camera: :camera: :camera: 신나는 레이싱! :camera: :camera: :camera:\n'
-			#racing_unit = [':giraffe:', ':elephant:', ':tiger2:', ':hippopotamus:', ':crocodile:',':leopard:',':ox:', ':sheep:', ':pig2:',':dromedary_camel:',':dragon:',':rabbit2:'] #동물스킨
-			#racing_unit = [':red_car:', ':taxi:', ':bus:', ':trolleybus:', ':race_car:', ':police_car:', ':ambulance:', ':fire_engine:', ':minibus:', ':truck:', ':articulated_lorry:', ':tractor:', ':scooter:', ':manual_wheelchair:', ':motor_scooter:', ':auto_rickshaw:', ':blue_car:', ':bike:', ':helicopter:', ':steam_locomotive:']  #탈것스킨
-			#random.shuffle(racing_unit) 
+			racing_unit = [':giraffe:', ':elephant:', ':tiger2:', ':hippopotamus:', ':crocodile:',':leopard:',':ox:', ':sheep:', ':pig2:',':dromedary_camel:',':dragon:',':rabbit2:'] #동물스킨
+			racing_unit = [':red_car:', ':taxi:', ':bus:', ':trolleybus:', ':race_car:', ':police_car:', ':ambulance:', ':fire_engine:', ':minibus:', ':truck:', ':articulated_lorry:', ':tractor:', ':scooter:', ':manual_wheelchair:', ':motor_scooter:', ':auto_rickshaw:', ':blue_car:', ':bike:', ':helicopter:', ':steam_locomotive:']  #탈것스킨
+			random.shuffle(racing_unit) 
 			racing_member = msg.split(" ")
 
 			racing_unit = []
@@ -2964,7 +2964,7 @@ class mainCog(commands.Cog):
 						result[i][1] = ':x:'
 					result_str += result[i][1] + "  " + result[i][0] + "  "
 					
-				#print(result)
+				print(result)
 				await asyncio.sleep(1)
 				return await result_race.edit(content = output + ':tada: 경주 종료!\n' + result_str)
 		else:
